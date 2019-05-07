f59e6d0c20f5d34def7e86b510a5ca72845142d8
PiyushBhangale <piyushbhangale1995@gmail.com>
2019-05-07 10:54:24 +0530

    LL del

---
+++ Linke_List/LL_deletion.py
@@ -0,0 +1,88 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def print_list(self):
+        point_node = self.head
+        while point_node:
+            print(point_node.data)
+            point_node = point_node.next
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def prepend(self, data):
+        new_node = Node(data)
+
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_after_node(self, prv_node, data):
+        if not prv_node:
+            print("node not present")
+            return
+        new_node = Node(data)
+        new_node.next = prv_node.next
+        prv_node.next = new_node
+
+    def delete_node(self, key):
+        current_node = self.head
+
+        if current_node and current_node.data == key:
+            self.head = current_node.next
+            current_node = None
+            return
+        prev_node = None
+        while current_node and current_node.data != key:
+            prev_node = current_node
+            current_node = current_node.next
+        if current_node == None:
+            return
+        prev_node.next = current_node.next
+        current_node = None
+
+    def del_node_at_pos(self, pos):
+        curr_node = self.head
+        counter = 0
+        if pos == 0:
+            self.head = curr_node.next
+            curr_node = None
+            return
+        prv_node = None
+        while curr_node and pos != counter:
+            prv_node = curr_node
+            curr_node = curr_node.next
+            counter += 1
+        if curr_node is None:
+            return
+        prv_node.next = curr_node.next
+        curr_node = None
+
+
+llist = LinkedList()
+llist.append("A")
+llist.append("B")
+llist.append("C")
+llist.prepend("D")
+llist.append("E")
+llist.append("G")
+
+llist.insert_after_node(llist.head.next, "F")
+llist.delete_node("F")
+llist.delete_node("D")
+llist.print_list()
+llist.del_node_at_pos(1)
+llist.print_list()
